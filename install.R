fd0a848b34495f54859d5fff551aa37a0740fc7f
Dominik Lindner <dominikl@users.noreply.github.com>
2019-08-29 16:39:50 +0100

    Update install.R

--- install.R
+++ install.R
@@ -1,4 +1,1 @@
-install.packages("curl", repos = "http://cran.us.r-project.org")
-install.packages("httr", repos = "http://cran.us.r-project.org")
-install.packages("methods", repos = "http://cran.us.r-project.org")
-install.packages("jpeg", repos = "http://cran.us.r-project.org")
+install.packages("https://github.com/ome/rOMERO-gateway/releases/download/v0.4.7/romero.gateway_0.4.7.tar.gz", repos = NULL, type = "source")
