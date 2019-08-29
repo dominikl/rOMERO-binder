0dd2e57d25d1bfc2bf3c0caa30f8a676dacc842c
Dominik Lindner <dominikl@users.noreply.github.com>
2019-08-29 15:38:52 +0100

    update install.R

--- install.R
+++ install.R
@@ -1,3 +1,4 @@
+install.packages("curl", repos = "http://cran.us.r-project.org")
 install.packages("httr", repos = "http://cran.us.r-project.org")
 install.packages("methods", repos = "http://cran.us.r-project.org")
 install.packages("jpeg", repos = "http://cran.us.r-project.org")
