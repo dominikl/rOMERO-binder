4d2ab634af7293e3eca3b4e9be7ac94cbb207c59
Dominik Lindner <dominikl@users.noreply.github.com>
2019-08-29 10:32:32 +0100

    Don't install romero.gateway for now

--- install.R
+++ install.R
@@ -6,5 +6,5 @@ install.packages("leaflet")
 install.packages("methods")
 install.packages("jpeg")
 install.packages("rJava")
-install.packages("https://github.com/ome/rOMERO-gateway/releases/download/v0.4.7/romero.gateway_0.4.7.tar.gz", repos = NULL, type = "source")
+#install.packages("https://github.com/ome/rOMERO-gateway/releases/download/v0.4.7/romero.gateway_0.4.7.tar.gz", repos = NULL, type = "source")
 
