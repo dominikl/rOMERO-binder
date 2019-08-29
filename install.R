a80bb581e22bde84e1df139a940cf700ed4d96c7
Dominik Lindner <dominikl@users.noreply.github.com>
2019-08-29 15:16:25 +0100

    Add romero installation again

--- install.R
+++ install.R
@@ -1,3 +1,4 @@
 install.packages("httr", repos = "http://cran.us.r-project.org")
 install.packages("methods", repos = "http://cran.us.r-project.org")
 install.packages("jpeg", repos = "http://cran.us.r-project.org")
+install.packages("https://github.com/ome/rOMERO-gateway/releases/download/v0.4.7/romero.gateway_0.4.7.tar.gz", repos = NULL, type = "source")
